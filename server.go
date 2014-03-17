d7ead05f40d776dcf7a4ce431acdbe2ae62a63cd
Robert Gogolok <gogolok@gmail.com>
2014-03-17 12:51:18 +0100

    helo anynines

--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import "github.com/codegangsta/martini"
 func main() {
   m := martini.Classic()
   m.Get("/", func() string {
-    s := []string{"Hello world! Go version:", runtime.Version()}
+    s := []string{"Hello anynines! Go version:", runtime.Version()}
     return strings.Join(s, " ")
   })
   m.Run()
