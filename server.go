21ddf3988161135482263953a12df7023cc0a10e
Robert Gogolok <gogolok@gmail.com>
2014-03-17 12:50:18 +0100

    output Go version

--- server.go
+++ server.go
@@ -1,11 +1,14 @@
 package main
 
+import "runtime"
+import "strings"
 import "github.com/codegangsta/martini"
 
 func main() {
   m := martini.Classic()
   m.Get("/", func() string {
-    return "Hello world!"
+    s := []string{"Hello world! Go version:", runtime.Version()}
+    return strings.Join(s, " ")
   })
   m.Run()
 }
